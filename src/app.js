3988a58517cbc3a2765237b48c83eac6669ef2b4
George Tada <george.6809@gmail.com>
2017-04-21 21:30:05 +0900

    コンポーネントを意識した構造にしてみた

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import styles from './css/style.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Hello from './js/Hello'
+import Hello from './components/Hello'
 
 ReactDOM.render(
     <Hello/>,
